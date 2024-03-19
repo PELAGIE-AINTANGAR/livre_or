4cb7195807a320410cefe3507463ac8dba4d0b2c
PELAGIE-AINTANGAR <115146707+PELAGIE-AINTANGAR@users.noreply.github.com>
2024-03-19 03:43:13 +0100

    Update connexion_base.php

--- connexion_base.php
+++ connexion_base.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+include_once 'config.php';
 class database{
     private $serveur;
     private $username;
@@ -6,10 +7,10 @@ class database{
     private $dbname;
 
     public function __construct($serveur,$username,$password,$dbname){
-        $this->serveur=$serveur;
-        $this->username=$username;
-        $this->password=$password;
-        $this->dbname=$dbname;
+        $this->serveur = DB_SERVER;
+        $this->username = DB_USERNAME;
+        $this->password = DB_PASSWORD;
+        $this->dbname = DB_NAME;
     }
 
     public function connect(){
