4193e3c8c41c89d4f506b450a1232d562d02d891
PELAGIE-AINTANGAR <115146707+PELAGIE-AINTANGAR@users.noreply.github.com>
2024-03-19 03:38:30 +0100

    Add files via upload

---
+++ config.exemple.php
@@ -0,0 +1,6 @@
+<?php
+//remplacez les valeurs par les votres et renommez le fichier config.php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'nom_base_de_donnees');
+define('DB_USER', 'nom_utilisateur');
+define('DB_PASS', 'mot_de_passe');
